aeb3a47ffe9c264b5e3f07863982d0bbdd5732cf
Kevin Willians Miguel <kevincursos@outlook.com>
2024-01-01 14:14:55 +0000

    Validations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,41 +10,37 @@ function onchangeEmail(){
 }
 
 function isEmailValid(){
-    const email = form.email.value;
+    const email = form.email().value;
     if (!email) {
         return false;
     }
     return validateEmail(email);
 }
 
-function isPasswordValid(){
-    const password = form.password.value;
-    if (!password){
-        return false;
-    }
-    return true;
-}
-
-function validateEmail(email) {
-    return /\S+@\S+\.\S+/.test(email);
-}
-
 function toggleEmailerrors() {
-    const email = form.email.value;
-    form.emailRequiredError().style.display = email ? "display" : "none";
-    form.emailInvalidError().style.display = validateEmail(email) ? "block" : "none";
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? "none" : "block";
+    form.emailInvalidError().style.display = validateEmail(email) ? "none" : "block";
 }
 
 function togglePassworderrors() {
-    const password = form.password.value;
-    form.passwordRequiredError().style.display = password ? "block" : "none";
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? "none" : "block";
 }
 
 function toggleButtonsDisable() {
     const emailValid = isEmailValid();
-    form.recoverPassword.disabled = !emailValid;
+    form.recoverPassword().disabled = !emailValid;
     const passwordValid = isPasswordValid();
-    form.loginButton.disabled = !emailValid || !passwordValid;
+    form.loginButton().disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid(){
+    const password = form.password().value;
+    if (!password){
+        return false;
+    }
+    return true;
 }
 
 const form = {
