1900170d46d2dacf068f7cf90f497b013d526bba
Kevinwmiguel <kevincursos@outlook.com>
2024-01-06 03:26:56 +0000

    home fakedata created

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,6 +6,52 @@ function logout(){
     })
 }
 
-const form = {
-    logout: () => document.getElementById('logout')
+findArticles();
+
+function findArticles() {
+    setTimeout(() => {
+        addArticlesToScreen(fakearticle)
+    }, 1000)
 }
+
+function addArticlesToScreen(articles){
+    const orderedList = document.getElementById('main-articles');
+
+    articles.forEach(article => {
+        const li = document.createElement('li');
+        li.classList.add(article.type);
+        
+        const date = document.createElement('p');
+        date.innerHTML = formatDate(article.date);
+        li.appendChild(date);
+
+        const type = document.createElement('p');
+        type.innerHTML = article.type;
+        li.appendChild(type);
+
+        if(article.descricao) {
+            const description = document.createElement('p');
+            description.innerHTML = article.descricao;
+            li.appendChild(description);
+        }
+
+        orderedList.appendChild(li);
+    });
+}
+
+function formatDate(date) {
+    return new Date(date).toLocaleDateString('pt');
+}
+
+const fakearticle = [{
+    type: 'article',
+    date: '2024-01-05',
+    title: 'titulo do artigo',
+    descricao: 'conteudo do meu artigo'
+},
+{
+    type: 'article',
+    date: '2024-01-05',
+    title: 'titulo do artigo',
+    descricao: 'conteudo do meu artigo'
+}]
